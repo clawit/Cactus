250feb64b3e95d0b1b5056b8def208c755bba8b3
shenyi <kevinyeti@gmail.com>
2019-12-02 17:27:33 +0800

    add RabbitBusTest

---
+++ src/Test/BusUnitTest/RabbitBusTest.cs
@@ -0,0 +1,37 @@
+﻿using Cactus.Bus.RabbitBus;
+using Cactus.Protocol.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BusUnitTest
+{
+    public class RabbitBusTest
+    {
+        private RabbitBus _bus = new RabbitBus(new Uri("rabbit://"), "Cactus");
+
+        [Fact]
+        public void PublishTest()
+        {
+            Packet packet = new Packet();
+            for (int i = 0; i < 10; i++)
+            {
+                _bus.Publish("Data", packet);
+            }
+        }
+
+        [Fact]
+        public void SubscribeTest()
+        {
+            _bus.Subscribe("Data", _processor);
+        }
+
+        private bool _processor(BusChannel channel, Packet packet)
+        {
+            return true;
+        }
+
+
+    }
+}
