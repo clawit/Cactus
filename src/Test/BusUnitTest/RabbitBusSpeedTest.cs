f582d94110230228f2ea8c4d13046840a5ee5355
KevinYeti <KevinYeti@gmail.com>
2019-12-03 15:26:32 +0800

    update unittest

--- src/Test/BusUnitTest/RabbitBusSpeedTest.cs
+++ src/Test/BusUnitTest/RabbitBusSpeedTest.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace BusUnitTest
@@ -26,12 +27,26 @@ namespace BusUnitTest
             packet.Args.Add(new string[] { "arg1", "arg2" });
             packet.Options.Add(new Dictionary<string, string>() { { "opt1", "1" }, { "opt2", "2" } });
 
+            for (int i = 0; i < 9; i++)
+            {
+                Task.Factory.StartNew(() => {
+                    var bus = new RabbitBus(new Uri("amqp://activator:activator@192.168.31.102:5672"), "Cactus");
+                    while (true)
+                    {
+                        bus.Publish("Event", packet);
+
+                        Thread.Sleep(0);
+                    }
+                });
+            }
+
             while (true)
             {
                 _bus.Publish("Event", packet);
 
-                Thread.Sleep(1000);
+                Thread.Sleep(0);
             }
+
         }
 
         private bool _processor(BusChannel channel, Packet packet)
