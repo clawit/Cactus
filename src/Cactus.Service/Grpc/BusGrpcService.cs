28d96072a9ccd0d542e3378eb7d6aea51df66162
KevinYeti <KevinYeti@gmail.com>
2019-12-05 00:25:07 +0800

    add BusGrpcService

---
+++ src/Cactus.Service/Grpc/BusGrpcService.cs
@@ -0,0 +1,37 @@
+﻿using Cactus.Protocol.Interface;
+using Cactus.Protocol.Model;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cactus.Service.Grpc
+{
+    public class BusGrpcService : BusService.BusServiceBase
+    {
+        private readonly ILogger<BusGrpcService> _logger;
+        public BusGrpcService(ILogger<BusGrpcService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<RequestResult> Subscribe(Packet packet, ServerCallContext context)
+        {
+            return Task.FromResult(new RequestResult
+            {
+                IsOk = true
+            });
+        }
+
+        public override Task<RequestResult> Publish(Packet packet, ServerCallContext context)
+        {
+            return Task.FromResult(new RequestResult
+            {
+                IsOk = true
+            });
+        }
+
+    }
+}
---
+++ src/Cactus.Service/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cactus.Service.Grpc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Cactus.Service
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<BusGrpcService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
